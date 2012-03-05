3e31460a9cabdad5dbed2a8ffce847176deb31d4
Aaron Gitlin <aarongitlin@gmail.com>
2012-03-05 19:34:34 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueDoorCafe::Application.config.secret_token = '7b1ca06d39788529d1df8e14b216ed17107e42ce5691bd4ebd58d90b33995528795ac5dcae8e32d595aadf88cd8d786e00400fbb370091a002c521b81974760a'

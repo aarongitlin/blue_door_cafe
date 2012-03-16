ac740535ca27858b023e36c51962d748961b04eb
Aaron Gitlin <aarongitlin@gmail.com>
2012-03-16 16:21:26 +1100

    deleting previously made pages controller, trying to get home page to work

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ BlueDoorCafe::Application.routes.draw do
   # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
   mount Refinery::Core::Engine, :at => '/'
 
-  get "pages/home"
+#  get "pages/home"
 
-  get "pages/contact"
+ # get "pages/contact"
   
-  get "pages/about"
+#  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -62,7 +62,7 @@ BlueDoorCafe::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#home'
+ # root :to => "pages#show"
 
   # See how all your routes lay out with "rake routes"
 

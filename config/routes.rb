1f5a7a8f189d70bbaaf5ee771eb6a1429ea66c0a
Aaron Gitlin <aarongitlin@gmail.com>
2012-03-17 13:12:49 +1100

    put routes.rb back to normal

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ BlueDoorCafe::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-root :to => "pages#show"
+  #  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 

9cdc39559efacba2ddf373d1bd126a731944d988
Aaron Gitlin <aarongitlin@gmail.com>
2012-03-22 14:18:58 +1100

    ready to deploy to heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ def title
 		"#{base_title} | #{@title}"
 	end
 end
+def custom_menu_branch_css(local_assigns)
+    options = local_assigns.dup
+    options.update(:sibling_count => options[:menu_branch].shown_siblings.length) unless options[:sibling_count]
+
+    css = []
+    css << 'active' if selected_page_or_descendant_page_selected?(local_assigns[:menu_branch])
+    css
+end
 end
